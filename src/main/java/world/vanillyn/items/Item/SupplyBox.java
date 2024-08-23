d937dbaeadf5f8c7b147d87f18c07660ba497e1b
vanillyn <vanillyn@multiryzz.ch>
2024-08-23 19:31:29 -0400

    user.writeNbt maybe?

--- src/main/java/world/vanillyn/items/Item/SupplyBox.java
+++ src/main/java/world/vanillyn/items/Item/SupplyBox.java
@@ -1,8 +1,12 @@
 package world.vanillyn.items.Item;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.util.ActionResult;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
 public class SupplyBox extends Item {
     public SupplyBox(Settings settings) {
@@ -10,8 +14,9 @@ public class SupplyBox extends Item {
     }
 
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        return super.useOnBlock(context);
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.writeNbt(new NbtCompound());
+        return null;
     }
 
 }
