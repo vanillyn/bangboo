7243811ce7dd5cd9b23c34518bc6e57069d83481
vanillyn <vanillyn@multiryzz.ch>
2024-08-27 19:40:17 -0400

    [patch] fix block.Block.SubHollow

--- src/main/java/world/vanillyn/block/Block/SubHollow.java
+++ src/main/java/world/vanillyn/block/Block/SubHollow.java
@@ -4,6 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import world.vanillyn.data.damage.Ether;
@@ -18,8 +20,19 @@ public class SubHollow extends Block {
         super.onEntityCollision(state, world, pos, entity);
 
         if (!world.isClient && entity instanceof PlayerEntity user) {
-            Ether ether = new Ether(null);
+            Ether ether = new Ether(null, null);
             user.damage(ether, 20f);
         }
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        super.onEntityCollision(state, world, pos, player);
+
+        if (!world.isClient) {
+            Ether ether = new Ether(null, player);
+            player.damage(ether, 20f);
+        }
+        return ActionResult.SUCCESS;
     }
 }
