8866c73d4358344b1d214b04a0cc1098a996f230
vanillyn <vanillyn@multiryzz.ch>
2024-08-27 19:59:04 -0400

    [remove] deleted data.damage.Ether, replace with BangbooDamage.ETHER

--- src/main/java/world/vanillyn/block/Block/SubHollow.java
+++ src/main/java/world/vanillyn/block/Block/SubHollow.java
@@ -3,12 +3,13 @@ package world.vanillyn.block.Block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import world.vanillyn.data.damage.Ether;
+import world.vanillyn.data.damage.BangbooDamage;
 
 public class SubHollow extends Block {
     public SubHollow(Settings settings) {
@@ -17,21 +18,17 @@ public class SubHollow extends Block {
 
     @Override
     public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        super.onEntityCollision(state, world, pos, entity);
-
-        if (!world.isClient && entity instanceof PlayerEntity user) {
-            Ether ether = new Ether(null, null);
-            user.damage(ether, 20f);
+        if (!world.isClient && entity instanceof PlayerEntity player) {
+            DamageSource ether = BangbooDamage.of(world, BangbooDamage.ETHER);
+            player.damage(ether, 20f);
         }
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        super.onEntityCollision(state, world, pos, player);
-
         if (!world.isClient) {
-            Ether ether = new Ether(null, player);
-            player.damage(ether, 20f);
+            DamageSource ether = BangbooDamage.of(world, BangbooDamage.ETHER);
+            player.damage(ether, 20.0f);
         }
         return ActionResult.SUCCESS;
     }
