16b4103cb424d928a796722bd58d8e4e0465557c
vanillyn <vanillyn@multiryzz.ch>
2024-08-27 20:13:48 -0400

    [patch] fix Ether damage and recipes

--- src/main/java/world/vanillyn/block/Block/SubHollow.java
+++ src/main/java/world/vanillyn/block/Block/SubHollow.java
@@ -4,7 +4,10 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
@@ -24,11 +27,25 @@ public class SubHollow extends Block {
         }
     }
 
+    public void etherDamage(World world, RegistryKey<DamageType> type, Entity entity, float amount) {
+        DamageSource ether = new DamageSource(
+                world.getRegistryManager()
+                        .get(RegistryKeys.DAMAGE_TYPE)
+                        .entryOf(type));
+
+        entity.damage(ether, amount);
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if (!world.isClient && entity instanceof PlayerEntity player) {
+            etherDamage(world, BangbooDamage.ETHER, player, 20f);
+        }
+    }
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (!world.isClient) {
-            DamageSource ether = BangbooDamage.of(world, BangbooDamage.ETHER);
-            player.damage(ether, 20.0f);
+            etherDamage(world, BangbooDamage.ETHER, player, 20f);
         }
         return ActionResult.SUCCESS;
     }
