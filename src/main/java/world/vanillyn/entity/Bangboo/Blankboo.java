48ccbfc680efe94b133b629586ec6339c7e89f05
vanillyn <vanillyn@multiryzz.ch>
2024-08-28 11:53:47 -0400

    [patch] fix entity.bangboo.Blankboo

--- src/main/java/world/vanillyn/entity/Bangboo/Blankboo.java
+++ src/main/java/world/vanillyn/entity/Bangboo/Blankboo.java
@@ -1,24 +1,19 @@
 package world.vanillyn.entity.bangboo;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.passive.TameableEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import world.vanillyn.BangbooMod;
 
 
 public class Blankboo extends Bangboo {
-    public Blankboo(EntityType<? extends TameableEntity> entityType, World world) {
+    public Blankboo(EntityType<? extends Bangboo> entityType, World world) {
         super(entityType, world);
     }
 
     @Override
-    public ActionResult interactMob(PlayerEntity player, Hand hand) {
-
-        return ActionResult.PASS;
+    public BangbooType<?> type() {
+        return null;
     }
 
     public Identifier getBangbooTexture() {
